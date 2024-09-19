ab22d18a648d6e43e04fca6cf50bdf83d98f5139
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-09-19 12:34:35 -0400

    chapter 3

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact Manager - Home</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+    </head>
+    <body>
+        <?php include("header.php"); ?>
+        <main>
+           
+        </main>
+        <?php include("footer.php"); ?>
+    </body> 
+</html>
